afc70049a5e18605f139a5acc5eb97488199f566
M4V3R1CK <eddymav247@gmail.com>
2022-06-24 14:19:29 +0100

    polls: update polls models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=255)
+    question_description = models.CharField(max_length=255, default="Basic Description")
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+   
+
+   
